db9da2b7b73f4270c009c2d6eee9bb707c8250fa
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-09-12 11:56:55 +0300

    Added author to comment and post models

--- backend/src/models/commentModel.js
+++ backend/src/models/commentModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     text: { type: String, required: true },
-    authorName: { type: String, required: true },
+    author: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
     likes: { type: Number, default: 0 },
 }, { timestamps: true });
 
--- backend/src/models/postModel.js
+++ backend/src/models/postModel.js
@@ -4,6 +4,7 @@ const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
     text: { type: String, required: true },
     likes: { type: Number, default: 0 },
+    author: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Post', postSchema);
