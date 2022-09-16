a0501d52cc07621441943a548640cf4954e27d23
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-09-16 12:19:28 +0300

    Added login and register routes and links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Navbar from "./components/Navbar/Navbar";
 import SinglePost from "./pages/SinglePost/SinglePost";
+import LoginPage from "./pages/Login/LoginPage";
+import RegisterPage from "./pages/Register/RegisterPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts/:postId" element={<SinglePost />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </>
   );
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,11 @@ export default function Navbar() {
     return (
         <nav>
             <h3 className="nav-title">Blog App</h3>
-            <Link className="nav-home-link" to="/"><h4 >Home</h4></Link>
+            <section className="nav-link-section">
+                <Link className="nav-home-link nav-link" to="/"><h4 >Home</h4></Link>
+                <Link className="nav-login-link nav-link" to="/login"><h4 >Log In</h4></Link>
+                <Link className="nav-register-link nav-link" to="/register"><h4 >Register</h4></Link>
+            </section>
         </nav>
     );
 }
